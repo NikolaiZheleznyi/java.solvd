5b60b90c7e7a0358437154a4409e0f86193ca2f7
MacBook <victoria_dragun@MacBook-Air-Victoria.local>
2021-06-03 17:24:53 +0300

    add switch case, Scanner, class Nomination

--- src/com/solvd/films/feature/Comedy.java
+++ src/com/solvd/films/feature/Comedy.java
@@ -12,35 +12,17 @@ public class Comedy extends Feature {
         setReleaseYear(2015);
         setDurationMinutes(121);
         setProducer("Nancy Myers");
-        setFunnyMoments(12);
     }
 
-    public Comedy(String name, int releaseYear, int durationMinutes, String producer, int funnyMoments){
+    public Comedy(String name, int releaseYear, int durationMinutes, String producer){
         super(name, releaseYear, durationMinutes, producer);
-        this.funnyMoments = funnyMoments;
     }
 
-    public int getFunnyMoments(){
-        return funnyMoments;
-    }
-    public void setFunnyMoments(int funnyMoments){
-        this.funnyMoments = funnyMoments;
-    }
 
     @Override
     public void printInfo(){
         System.out.println("Comedy");
-        System.out.println("Name: " + getName());
-        System.out.println("Release year: " + getReleaseYear() +" year");
-        System.out.println("Duration minutes: " + getDurationMinutes() + " minutes");
-        System.out.println("Producer: " + getProducer());
-        System.out.println("Funny moments in film: " + getFunnyMoments());
+
     }
-    @Override
-    public void indicateRating() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Describe how you like the film: ");
-        String num = in.nextLine();
-        System.out.println("You rate the film at: " + num);
-    }
+
 }
