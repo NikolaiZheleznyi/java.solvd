46d0b2dc7ffb80cc674559e35eba1bd8266b5414
MacBook <victoria_dragun@MacBook-Air-Victoria.local>
2021-06-08 18:56:18 +0300

    add class nomination

--- src/com/solvd/films/feature/Nomination.java
+++
@@ -1,9 +0,0 @@
-package com.solvd.films.feature;
-import com.solvd.films.feature.Rating;
-import com.solvd.films.feature.Feature;
-import com.solvd.films.feature.Comedy;
-public class Nomination extends Rating {
-        public void nomination(Comedy feature){
-                
-        }
-}
